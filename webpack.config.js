0e8d69ee69c6399ad0757bc319ffb0374e6628fc
Vika Abysova <s.vika87@ukr.net>
2023-04-04 11:22:38 +0300

    delete comments in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,16 +4,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const devServerWebpack = require('./webpack.dev-server');
 const resolveWebpack = require('./webpack.resolve');
 const moduleWebpack = require('./webpack.module');
-// const { default: devServerWebpack } = require('./webpack.dev-server');
-// const { default: resolveWebpack } = require('./webpack.resolve').default;
-// const { default: moduleWebpack } = require('./webpack.module');
-// const { default: devServer } = require('./webpack.dev-server');
-// const { default: resolve } = require('./webpack.resolve');
-// const { default: moduleWebpack } = require('./webpack.module');
-// import devServer from './webpack.dev-server.js';
-// import './webpack.dev-server.js';
-// import './webpack.module.js';
-// import './webpack.resolve.js';
 
 module.exports = {
   output: {
