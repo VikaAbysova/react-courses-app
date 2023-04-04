830177d1038d55727d11084a32a9858249bb35f5
Vika Abysova <s.vika87@ukr.net>
2023-04-04 11:33:05 +0300

    change eslint acording to  home-task-1

--- webpack.config.js
+++ webpack.config.js
@@ -2,46 +2,46 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-	output: {
-		path: path.join(__dirname, '/dist'),
-		filename: 'index.bundle.js',
-	},
-	devServer: {
-		static: path.resolve(__dirname, 'src'),
-		port: 3000,
-		open: true,
-		hot: true,
-	},
-	resolve: {
-		extensions: ['.js', '.jsx', '.ts', '.tsx'],
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-				},
-			},
-			{
-				test: /\.(scss)$/,
-				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-			},
-			{
-				test: /\.(png|jpe?g|gif)$/i,
-				use: [
-					{
-						loader: 'file-loader',
-					},
-				],
-			},
-			{
-				test: /\.js$/,
-				enforce: 'pre',
-				use: ['source-map-loader'],
-			},
-		],
-	},
-	plugins: [new MiniCssExtractPlugin()],
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index.bundle.js',
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'src'),
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(scss)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        use: ['source-map-loader'],
+      },
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin()],
 };
