7e00c0cb59045059e93ded69f436caa24a4b2f5b
Vika Abysova <s.vika87@ukr.net>
2023-04-09 19:54:55 +0300

    ht4

--- src/common/Button/Button.jsx
+++ src/common/Button/Button.jsx
@@ -5,14 +5,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export const Button = ({ text, onClick, type, id }) => (
-	<button type={type} onClick={onClick} id={id}>
-		{text}
-	</button>
+  <button type={type} onClick={onClick} id={id}>
+    {text}
+  </button>
 );
 
 Button.propTypes = {
-	text: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
-	onClick: PropTypes.func,
-	type: PropTypes.oneOf(['submit', 'button']),
-	id: PropTypes.string,
+  text: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+  onClick: PropTypes.func,
+  type: PropTypes.oneOf(['submit', 'button']),
+  id: PropTypes.string,
 };
